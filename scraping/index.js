d06ea2c8cb505224bcad2825ca40f43251f45079
nATOr <gowluonade@gmail.com>
2023-01-10 12:13:03 -0500

    first scraping

--- scraping/index.js
+++ scraping/index.js
@@ -2,4 +2,4 @@ import * as cheerio from 'cheerio'
 const res = await fetch('https://kingsleague.pro/estadisticas/clasificacion/')
 const text = await res.text()
 const $ = cheerio.load(text)
-const table = $('table tbody tr').each((i,el)=>{console.log( $(el).text())})
+const table = $('table tbody tr').each((i,el)=>{console.log( $(el).text())}) // This is the problem
