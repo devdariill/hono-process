7cbee953abb9e31359c2f00d7d81bccf17e67e05
nATOr <gowluonade@gmail.com>
2023-01-10 12:47:48 -0500

    cleantext

--- scraping/index.js
+++ scraping/index.js
@@ -1,37 +1,34 @@
-import * as cheerio from "cheerio";
+import * as cheerio from 'cheerio'
 const URLS = {
-    leaderboard: "https://kingsleague.pro/estadisticas/clasificacion/",
+  leaderboard: 'https://kingsleague.pro/estadisticas/clasificacion/'
 }
-async function scrape(url) {
-    const res = await fetch(url);
-    const html = await res.text();
-    return cheerio.load(html);
+async function scrape (url) {
+  const res = await fetch(url)
+  const html = await res.text()
+  return cheerio.load(html)
 }
-async function getLeaderBoard(){
-    const $ = await scrape(URLS.leaderboard);
-    $("table tbody tr").each((i, el) => {
-        const rawTeam = $(el).find(".fs-table-text_3").text();
-        const rawVictories = $(el).find(".fs-table-text_4").text();
-        const rawDefeats = $(el).find(".fs-table-text_5").text();
-        const rawGoalsScored = $(el).find(".fs-table-text_6").text();
-        const rawGoalsAgainst = $(el).find(".fs-table-text_7").text();
-  const rawYellowCards = $(el).find(".fs-table-text_8").text();
-  const rawRedCards = $(el).find(".fs-table-text_9").text();
-  console.log({
-      rawTeam,
-      rawVictories,
-    });
-});
+const cleanText = (text) => text.replace(/\t|\n|\s:/g, '').replace(/.*:/g, ' ')
+async function getLeaderBoard () {
+  const $ = await scrape(URLS.leaderboard)
+  const $rows = $('table tbody tr')
+  $rows.each((i, el) => {
+    const $el = $(el)
+    const rawTeam = $el.find('.fs-table-text_3').text()
+    const rawVictories = $el.find('.fs-table-text_4').text()
+    const rawDefeats = $el.find('.fs-table-text_5').text()
+    const rawGoalsScored = $el.find('.fs-table-text_6').text()
+    const rawGoalsAgainst = $el.find('.fs-table-text_7').text()
+    const rawYellowCards = $el.find('.fs-table-text_8').text()
+    const rawRedCards = $el.find('.fs-table-text_9').text()
+    console.log(
+      cleanText(rawTeam),
+      cleanText(rawVictories),
+      cleanText(rawDefeats),
+      cleanText(rawGoalsScored),
+      cleanText(rawGoalsAgainst),
+      cleanText(rawYellowCards),
+      cleanText(rawRedCards)
+    )
+  })
 }
-getLeaderBoard();
-const leaderboard = [
-  {
-    team: "Team 1",
-    wins: 0,
-    loses: 0,
-    goalsScored: 0,
-    goalsAgainst: 0,
-    cardsYellow: 0,
-    cardsRed: 0,
-  },
-];
+getLeaderBoard()
