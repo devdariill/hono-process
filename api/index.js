f169df282d385edb8376092265a7fd493764e044
nATOr <gowluonade@gmail.com>
2023-01-10 13:57:57 -0500

    config Hono

--- api/index.js
+++ api/index.js
@@ -1,19 +1,33 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono'
 import leadeboard from '../db/leaderboard.json'
-export default {
-  async fetch (request, env, ctx) {
-    return new Response(JSON.stringify(leadeboard), {
-      headers: {
-        'content-type': 'application/json;charset=UTF-8'
-      }
-    })
-  }
-}
+// export default {
+//   async fetch (request, env, ctx) {
+//     return new Response(JSON.stringify(leadeboard), {
+//       headers: {
+//         'content-type': 'application/json;charset=UTF-8'
+//       }
+//     })
+//   }
+// }
+const app = new Hono()
+
+app.get('/', (ctx) => {
+  return ctx.json([
+    {
+      endpoint: '/leaderboard',
+      description: 'leaderboard'
+    }
+  ])
+})
+
+app.get('/leaderboard', (ctx) => {
+  return ctx.json([
+    {
+      endpoint: '/',
+      description: 'home'
+    },
+    leadeboard
+  ])
+})
+
+export default app
